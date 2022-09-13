9b766db66a757a2198af06a76191cb40f27993bb
Lukas Meinhart <100217325+dowsn@users.noreply.github.com>
2022-09-13 19:25:26 +0000

    Date api

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // index.js
 // where your node app starts
-
 // init project
 var express = require('express');
 var app = express();
@@ -20,13 +19,29 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// app.get("/api/hello", function (req, res) {
+//   res.json({greeting: 'hello API'});
+// });
 
+app.get("api/:date?", function(req, res){
+  let date = req.params.date;
+  let data;
+ 
+  if(date.length === 0) {
+  date = Date.now()
+  }
 
+   let utc = new Date(date).toString();
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+if(!utc) {
+  data = {
+    error : "Invalid Date"
+  }
+  res.json(data);
+}
+  
+let unix = date.indexOf('-') === (-1) ? date  : Math.floor(date.getTime())
+                                                           data = {
+    utc, unix                                            };
+  res.json(data);
+})
